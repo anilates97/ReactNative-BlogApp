9045daa5349571f36e47f8339b37cf53419b7632
anilates97 <anilates.97@gmail.com>
2023-12-03 18:33:54 +0300

    app iniate

---
+++ screens/HomeScren.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function HomeScren() {
+  return (
+    <View>
+      <Text>HomeScren</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
